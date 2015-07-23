2347e1796e9f5fcbe3979fdbdcc3a080f33a1410
Ni Zhihao <niz@rose-hulman.edu>
2015-07-22 22:03:27 -0700

    simple update

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ var app = express();
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
