c87dd0b6b7831279b1fcf75106f9f276ac767153
Ni Zhihao <niz@rose-hulman.edu>
2015-07-22 22:55:02 -0700

    rm comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,9 +30,6 @@ var CommentBox = React.createClass({
     var comments = this.state.data;
     comments.push(comment);
     this.setState({data: comments}, function() {
-      // `setState` accepts a callback. To avoid (improbable) race condition,
-      // `we'll send the ajax request right after we optimistically set the new
-      // `state.
       $.ajax({
         url: this.props.url,
         dataType: 'json',
@@ -69,9 +66,6 @@ var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment, index) {
       return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
         <Comment author={comment.author} key={index}>
           {comment.text}
         </Comment>
